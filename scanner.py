d11a2be87d2edcf22484b9f92a00773026318b62
AndreevDaniil2002 <90094074+AndreevDaniil2002@users.noreply.github.com>
2021-10-20 20:13:28 +0300

    Сканнер

--- scanner.py
+++ scanner.py
@@ -1,31 +1,46 @@
 import socket
-from threading import Thread
+import threading
+import time
 
-N = 2**16 - 1
-ports = []
-
-
-def scan(ip, start, finish):
-    for port in range(start, finish):
-        sock = socket.socket()
-        try:
-            sock.connect((ip, port))
-            # print("Порт", port, "открыт")
-            ports.append(port)
-            msg = 'scanner_command'
-            sock.send(msg.encode())
-        except:
-            continue
-        finally:
-            sock.close()
+ip = '127.0.0.1'
+thred_array = []
+closed_array = []
 
 
-def mainn(ip, amount_of_ports):
-    i_step = (N - 1024)//amount_of_ports
-    # print(i_step)
-    for i in range(1024, N, i_step):
-        t = Thread(target=scan, args=(ip, i, i+i_step))
-        t.start()
+def port_scan(ip, port_name):
+    global thred_array, closed_array
+    sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connect = sockt.connect((ip, port_name))
+        print(f'Порт: {port_name} открыт')
+        thred_array.append(port_name)
+        sockt.close()
+    except:
+        print(f'Порт: {port_name} закрыт')
+        closed_array.append(port_name)
+        sockt.close()
 
 
-# mainn('127.0.0.1', 1000)
+def open_ports():
+    global thred_array
+    print('Открытые порты: ')
+    print(thred_array)
+
+
+def closed_ports():
+    global closed_array
+    closed_array.sort()
+    print('Закрытые порты: ')
+    print(closed_array)
+
+
+for p_name in range(3000):
+    thred = threading.Thread(target=port_scan, args=(ip, p_name))
+    thred.start()
+
+time.sleep(3)
+sec_thred = threading.Thread(target=open_ports)
+sec_thred.start()
+
+thrd_th = threading.Thread(target=closed_ports)
+thrd_th.start()
